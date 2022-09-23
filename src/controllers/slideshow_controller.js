f6b49960f309b43b6728008fd12804a7e8503411
Hugo Oliveira <hbliveira@gmail.com>
2022-09-23 17:58:47 -0300

    feat: reading inital state from the DOM

--- src/controllers/slideshow_controller.js
+++ src/controllers/slideshow_controller.js
@@ -2,27 +2,32 @@ import { Controller } from "@hotwired/stimulus";
 
 
 export default class extends Controller {
+	// Stimulus controllers support typed value properties which automatically map to data attributes.
+	// Stimulus will create a this.indexValue controller property associated
+	// with a data-slideshow-index-value attribute, and handle the numeric conversion for us.
+	// https://stimulus.hotwired.dev/reference/values
+	static values = { index: Number }
+	
 	static targets = ["slide"]
 	
 	// https://stimulus.hotwired.dev/reference/lifecycle-callbacks
 	initialize() {
-		this.index = 0
 		this.showCurrentSlide()
 	}
 	
 	next() {
-		this.index++
+		this.indexValue++
 		this.showCurrentSlide()
 	}
 	
 	previous() {
-		this.index--
+		this.indexValue--
 		this.showCurrentSlide()
 	}
 	
 	showCurrentSlide() {
 		this.slideTargets.forEach((element, index) => {
-			element.hidden = index != this.index
+			element.hidden = index != this.indexValue
 		})
 	}
 }
