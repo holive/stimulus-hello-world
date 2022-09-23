00787e18d363cb9f62938b6afdd107c3f45b7acd
Hugo Oliveira <hbliveira@gmail.com>
2022-09-23 16:53:11 -0300

    feat: control class of an html element

--- src/controllers/clipboard_controller.js
+++ src/controllers/clipboard_controller.js
@@ -4,6 +4,13 @@ export default class extends Controller {
 	// For each target name in the array, Stimulus adds three new properties to your controller:
 	// this.sourceTarget, this.sourceTargets, this.hasSourceTarget
 	static targets = ["source"]
+	static classes = ["supported"]
+	
+	connect() {
+		if ("clipboard" in navigator) {
+			this.element.classList.add(this.supportedClass);
+		}
+	}
 	
 	copy(event) {
 		event.preventDefault()
