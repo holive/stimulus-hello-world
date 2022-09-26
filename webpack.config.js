08a51da996564fb39e6278b5cf1a1522353b35c5
Hugo Oliveira <hbliveira@gmail.com>
2022-09-26 18:59:57 -0300

    chore: configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,41 @@
 const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: {
-    bundle: "./src/index.js"
-  },
-
-  output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "public")
-  },
-
-  mode: "production",
-  devtool: "source-map",
-
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: [
-          /node_modules/
-        ],
-        use: [
-          { loader: "babel-loader" }
-        ]
-      }
-    ]
-  }
+	entry: {
+		bundle: "./src/index.js"
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'public/index.html'
+		}),
+		new CopyPlugin({
+			patterns: [
+				{ from: "public/*.css", to: "." },
+			],
+		}),
+	],
+	output: {
+		filename: "[name].js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+	
+	mode: "production",
+	devtool: "source-map",
+	
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: [
+					/node_modules/
+				],
+				use: [
+					{ loader: "babel-loader" }
+				]
+			},
+		]
+	}
 }
