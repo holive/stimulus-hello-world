e7d3e4dba4ecc51b652c807f25780aa9a4097c0e
Hugo Oliveira <hbliveira@gmail.com>
2022-09-30 17:20:46 -0300

    feat: finish circles animation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { Application } from "@hotwired/stimulus"
 import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 import '../public/main.css'
+import lazyStyles from '../public/styles.lazy.css'
+
+lazyStyles.use()
 
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
