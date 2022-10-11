754b7d13158a9f49ba8d04a0472914b830d7dadc
Hugo Oliveira <hbliveira@gmail.com>
2022-10-11 18:16:07 -0300

    feat: lazy load and minimize css

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,56 @@
 const path = require("path")
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HTMLInlineCSSWebpackPlugin = require("html-inline-css-webpack-plugin").default;
 
 module.exports = {
 	devServer: {
-		static: {
-			directory: path.join(__dirname, 'public'),
-		},
 		compress: true,
 		port: 9000,
+		static: { directory: path.join(__dirname, 'public') },
 	},
-	
-	entry: {
-		bundle: "./src/index.js"
-	},
-	output: {
-		filename: "[name].js", path: path.resolve(__dirname, "dist"), clean: true,
-	},
-	
+	entry: { bundle: "./src/index.js" },
+	output: { filename: "[name].js", path: path.resolve(__dirname, "dist"), clean: true },
 	mode: "production",
-	devtool: "source-map",
-	
 	module: {
 		rules: [
 			{
-				test: /\.css$/i, use: [
-					{
-						loader: 'style-loader',
-						options: {
-							insert: 'head',
-							injectType: 'singletonStyleTag'
-						},
-					},
-					"css-loader"
+				test: /\.css$/,
+				exclude: /\.lazy\.css$/i,
+				use: [ MiniCssExtractPlugin.loader, "css-loader" ],
+			},
+			{
+				test: /\.lazy\.css$/i,
+				use: [
+					{ loader: 'style-loader', options: { injectType: 'lazyStyleTag' } },
+					'css-loader',
 				],
 			},
 			{
 				test: /\.js$/,
 				exclude: [/node_modules/],
-				use: [{ loader: "babel-loader" }]
+				use: [{ loader: "babel-loader" }],
 			},
 			{
 				test: /\.ejs$/i, use: ['html-loader', 'template-ejs-loader'],
 			}
 		]
 	},
-	
+	optimization: {
+		minimizer: [ `...`, new CssMinimizerPlugin() ],
+	},
 	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+			chunkFilename: "[id].css",
+		}),
 		new HtmlWebpackPlugin({
 			filename: 'index.html', template: './src/ejs/index.ejs',
-		})
+			minify: true,
+			showErrors: true,
+			favicon: "public/favicon.ico"
+		}),
+		new HTMLInlineCSSWebpackPlugin(),
 	],
 }
