e943be4255628b416fa9d43ec3b06e183c89ce5f
Hugo Oliveira <hbliveira@gmail.com>
2022-10-11 15:05:42 -0300

    feat: add text animation

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 import '../public/main.css'
 import lazyStyles from '../public/styles.lazy.css'
 
-lazyStyles.use()
+lazyStyles?.use()
 
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
